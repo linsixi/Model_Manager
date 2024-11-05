a91ef96f4f26bf3d24b13e7425e78092f5f7606a
linsixi <1205631050@qq.com>
2024-11-05 21:31:13 +0800

    增加cpu_usage路由

--- web.py
+++ web.py
@@ -6,6 +6,7 @@ import uvicorn
 import json
 from read_json import read_json
 import numpy as np
+import psutil
 
 app = FastAPI()
 app.add_middleware(
@@ -34,7 +35,20 @@ async def put_path(model_request: Request):
     return value
 
 
+@app.get("/get_cpu_usage")
+async def get_cpu_usage():
+    # 获取CPU的总使用率
+    cpu_usage = psutil.cpu_percent(interval=1)
+    print(f"CPU使用率: {cpu_usage}%")
+    # 计算总使用率
+    cpu_cores_usage = psutil.cpu_percent(percpu=True, interval=1)
+    total_cpu_usage = sum(cpu_cores_usage)
+    print(f"全部CPU核心总使用率: {total_cpu_usage}% (可超过100%)")
+    return cpu_usage
+
+
 # 主监听函数
 if __name__ == "__main__":
-    uvicorn.run(app="web:app", host="192.168.0.240", port=8080, reload=False)  # 华为云
+    uvicorn.run(app="web:app", host="10.21.56.118", port=11451, reload=False)  # QG118
+    # uvicorn.run(app="web:app", host="10.21.56.119", port=11451, reload=False)  # QG119
     # uvicorn.run(app="web:app", host="127.0.0.1", port=8080, reload=False)
